0c7c55f791d2ff6cff498d93df6e8435c1e2c27a
Abel Zazueta <abel.zazueta@gmail.com>
2022-05-27 14:15:55 -0700

    Assignment completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+
+import React from "react";
+function Keypad() {
+    function passwordChange(event) {
+        console.log("Entering password...");
+    }
+    return (
+        <input type="password" onChange={passwordChange}></input>
+    );
+}
+
+export default Keypad;
